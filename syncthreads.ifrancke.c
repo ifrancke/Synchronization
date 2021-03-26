aba14c21d9c80202d26406887f941224c9c0650f
ifrancke <ifrancke@uvm.edu>
2021-03-26 12:44:45 -0400

    Fixed counter problem

--- syncthreads.ifrancke.c
+++ syncthreads.ifrancke.c
@@ -5,9 +5,7 @@
 #define NUMTHREADS 3
 #define MAXVAL 40
 pthread_mutex_t mutex1;
-pthread_mutex_t mutex2;
-pthread_mutex_t mutex3;
-int counter = 1;
+int counter = 0;
 
 typedef struct {
     int startVal;
@@ -67,8 +65,6 @@ int main() {
     CountStruct counts[NUMTHREADS];
     pthread_t threads[NUMTHREADS];
     pthread_mutex_init(&mutex1, NULL);
-    pthread_mutex_init(&mutex2, NULL);
-    pthread_mutex_init(&mutex3, NULL);
 
     for(int i = 0; i < NUMTHREADS; i++){
         counts[i].startVal = i;
